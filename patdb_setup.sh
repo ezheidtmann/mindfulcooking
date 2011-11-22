7e6f610dddc172a7d2578099398dbcd7922596c4
Evan Heidtmann <evan.heidtmann@gmail.com>
2011-11-21 16:05:59 -0800

    Enable toolbar; disable management menu block

--- patdb_setup.sh
+++ patdb_setup.sh
@@ -9,11 +9,14 @@ drush --yes si minimal \
   --site-name="PAT Membership Database" \
   --site-mail=evan.heidtmann@gmail.com
 
+drush --yes en contextual toolbar overlay
+
 drush --yes en patdb_structure
 drush --yes en patdb_views
 drush --yes en patdb_misc
 drush --yes en patdb
 drush --yes en patdb_import
+
 
 echo "Next step: Load an MDB! Ex: drush ipm ~/pat.mdb"
 
